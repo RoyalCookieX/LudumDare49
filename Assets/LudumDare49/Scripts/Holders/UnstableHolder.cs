80c7e918143c5db4aac14a8f61c674199ea63836
Andrei Cabungcal <40614905+RoyalCookieX@users.noreply.github.com>
2021-10-03 16:32:46 -0500

    Finished Unstable Shapes

--- Assets/LudumDare49/Scripts/Holders/UnstableHolder.cs
+++ Assets/LudumDare49/Scripts/Holders/UnstableHolder.cs
@@ -1,13 +1,24 @@
+using System;
+using Cinemachine;
 using NaughtyAttributes;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
+[RequireComponent(typeof(CinemachineImpulseSource))]
 public class UnstableHolder : MonoBehaviour, IObjectHolder<PlayerHoldable>
 {
     public PlayerHoldable Holdable => _holdable;
+
     
     [BoxGroup("Properties"), SerializeField] private float _releaseOffset = 6f;
 
     private PlayerHoldable _holdable;
+    private CinemachineImpulseSource _impulseSource;
+
+    private void Start()
+    {
+        _impulseSource = GetComponent<CinemachineImpulseSource>();
+    }
 
     private void OnCollisionEnter(Collision other)
     {
@@ -24,6 +35,7 @@ public class UnstableHolder : MonoBehaviour, IObjectHolder<PlayerHoldable>
         _holdable = holdable;
         _holdable.Hold();
         Release(transform.position, Random.insideUnitCircle.normalized, _releaseOffset);
+        _impulseSource.GenerateImpulse();
     }
 
     public void Release(Vector2 startPos, Vector2 throwDir, float releaseOffset)
