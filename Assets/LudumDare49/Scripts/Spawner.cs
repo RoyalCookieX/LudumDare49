11c7a9bda89f99f9f39fa48f41cd2882ada25983
Andrei Cabungcal <40614905+RoyalCookieX@users.noreply.github.com>
2021-10-03 00:40:51 -0500

    Reworked Spawner

--- Assets/LudumDare49/Scripts/Spawner.cs
+++ Assets/LudumDare49/Scripts/Spawner.cs
@@ -1,47 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
     public List<GameObject> _spawnObjects;
+    public float spawnRadius;
     public GameObject player;
     public bool stopSpawn = false;
 
     public float minTime = 1;
     public float maxTime = 10;
-    public int range = 10;
 
     void Start()
     {
         StartCoroutine(SpawnObject());
     }
-    public IEnumerator SpawnObject()
+    
+    private IEnumerator SpawnObject()
     {
-        float spawnTime;
+        yield return null;
+        while (true)
+        {
+            if (!stopSpawn)
+            {
+                Vector3 spawnPos = transform.position + (Vector3)Random.insideUnitCircle * spawnRadius;
+                GameObject spawnObject = _spawnObjects[Random.Range(0, _spawnObjects.Count)];
+                Instantiate(spawnObject, spawnPos, Quaternion.identity);
+            }
+            yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+        }
+    }
 
-        spawnTime = Random.Range(minTime, maxTime);
-        GameObject spawnObject = _spawnObjects[Random.Range(0, _spawnObjects.Count)];
-        
-
-        if ((player.transform.position - this.transform.position).sqrMagnitude < range * range)
-        {
-            stopSpawn = true;
-        }
-        else
-        {
-            stopSpawn = false;
-        }
-
-        yield return new WaitForSeconds(spawnTime);
-        if (stopSpawn)
-        {
-            //Stop spawning
-        }
-        else
-        {
-            Instantiate(spawnObject, this.transform.position, this.transform.rotation);
-        }
-        StartCoroutine(SpawnObject());
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, spawnRadius);
     }
 }
